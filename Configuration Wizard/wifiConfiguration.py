605d4163efc9cf0067d4b245cb5d5db18dd10f18
juanj <juanjmontoya14@gmail.com>
2020-05-29 18:34:40 -0500

    Wifi List UI

--- Configuration Wizard/wifiConfiguration.py
+++ Configuration Wizard/wifiConfiguration.py
@@ -2,17 +2,28 @@ import re
 import subprocess
 
 def getsidds():
-    results = subprocess.check_output(["netsh","wlan","show","Network"])
+    results = subprocess.check_output(["netsh","wlan","show","Network",  "mode=Bssid"])
     results = str(results).replace("\\r","")
     results = results.split("\\n")
-
     results = results[4:]
 
     sidds = []
-
+    intencities = []
     for x in results:
+        print(x)
         if ":" in x and not "    " in x:
             name = re.search("[^:]*", x)
             if len(x.replace(name[0],"")[2:]):
                 sidds.append(x.replace(name[0],"")[2:])
-    return(sidds)
+        elif "Signal" in x:
+            name = re.search("[^:]*", x)
+            if len(x.replace(name[0],"")[2:]):
+                intencities.append(x.replace(name[0],"")[2:])
+    nets = []
+    for n in range(0,len(sidds)):
+        data = {
+            "name": sidds[n],
+            "intencity": intencities[n]
+        }
+        nets.append(data)
+    return(nets)
